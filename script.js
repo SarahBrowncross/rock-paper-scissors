cbcee2669860645e9cac5814afdbc488c73544ee
Sarah Browncross <sarah.browncross@gmail.com>
2020-05-14 12:15:41 +0100

    computerPlay function complete

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("Hello, I am working");
+
+
+function computerPlay() {
+    let generator = Math.random() * 3;
+    if (generator < 1) {
+        return "rock";
+    }
+    else if (generator >= 1 && generator < 2) {
+        return "paper";
+    }
+    else {
+        return "scissors";
+    }
+}
+
+console.log(computerPlay());
+
+
