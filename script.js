5725e8e77c0a9fadf079435c0056b243cf371d6a
Sarah Browncross <sarah.browncross@gmail.com>
2020-05-14 18:29:51 +0100

    Game with five rounds

--- script.js
+++ script.js
@@ -13,24 +13,52 @@ function computerPlay() {
     }
 }
 
-let playerPlay = prompt("Choose rock, paper or scissors");
-playerPlay = playerPlay.toLowerCase();
+
+
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-        return "it's a draw";
+        return 0;
     }
     else if (playerSelection === "rock" && computerSelection === "scissors" || playerSelection === "scissors" && computerSelection === "paper" || playerSelection === "paper" && computerSelection === "rock") {
-        return "You win!";
+        return 1;
     }
     else {
-        return "You lose"
+        return -1;
     }
 
 }
 
-const computerSelection = computerPlay()
-console.log(computerSelection);
-console.log(playerPlay);
-console.log(playRound(playerPlay, computerSelection));
+let winCount = 0;
+let gameCount = 5;
+
+for (let i = 0; i < gameCount; i++) {
+    let playerPlay = prompt("Choose rock, paper or scissors");
+    playerPlay = playerPlay.toLowerCase();
+    const computerSelection = computerPlay()
+    let result = playRound(playerPlay, computerSelection);
+    winCount = winCount + result;
+    if (result === 0) {
+        console.log("draw");
+    }
+    else if (result === 1) {
+        console.log("win");
+    }
+    else if (result === -1) {
+        console.log("lose");
+    }
+}
+
+if (winCount === 0) {
+    console.log("It's a draw");
+}
+else if (winCount > 0) {
+    console.log("You win!");
+}
+else {
+    console.log("You lose");
+}
+
+
+
 
